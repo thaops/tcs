a2ea6b02da41f417e5ae06fcc186e7df23f3e660
robinD35 <127478689+thaops@users.noreply.github.com>
2025-10-28 15:36:26 +0700

    update domain app api-tcshr.azurewebsites.net version 1.0.20

--- lib/common/Services/config.dart
+++ lib/common/Services/config.dart
@@ -8,7 +8,7 @@ class Config {
   static const String _manualEnvKey = 'manual_environment_set';
   // Default URLs
   static const String _defaultProdBaseUrl =
-      "https://api-tcs-uat.azurewebsites.net/api";
+      "https://api-tcshr.azurewebsites.net/api";
   // "https://namphuong-api.azurewebsites.net/api";
   static const String _defaultDevBaseUrl =
       "https://namphuong-api-dev.azurewebsites.net/api";
